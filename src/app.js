5f1357df3a19580ebed243e9f5336f6cd873727c
bmafnas <bmafnas@gmail.com>
2023-03-17 22:42:52 -0700

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -25,20 +25,27 @@ function formatDate(timestamp) {
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition[0].description;
+  descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "939f4dt4a1ab705bfb2coa9efca9e8d1";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={San Francisco}&key=${apiKey}&unit=metric`;
+let city = "San Francisco";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
